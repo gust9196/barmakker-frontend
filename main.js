2df8f6a190009f11c4671701e991e6bc6d19e23e
frizzy1209 <kloecker1997@live.dk>
2022-11-22 20:27:46 +0100

    Har fået det hele samlet på en hjemmeside

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 
+/*
 
 fetch('https://barmakker-api.onrender.com/johnny')
     .then(response => response.json())
@@ -30,6 +31,8 @@ function renderBars(bars) {
     }
 }
 
+
+ */
 
 //brugeren skal kunne tilføje en bar til databasen
 
@@ -101,14 +104,47 @@ fetch('http://localhost:3000/user/create', {
 // Find din bar
 
 /*
-window.open('findbar.html')
+ window.open('findbar.html')
  */
+
+
 
 const theWebPage = document.querySelector('main')
 const findBarBtn = document.querySelector('.findBar button')
 
 findBarBtn.addEventListener('click', function () {
-    window.open('findbar.html')
+    theWebPage.innerHTML = " "
+    
+
+    fetch('https://barmakker-api.onrender.com/johnny')
+        .then(response => response.json())
+        .then(function (bars) {
+            renderBars(bars);
+        });
+
+    function renderBars(bars) {
+        const ul = document.querySelector('ul#bars');
+        console.log(ul);
+        for (let i = 0; i < bars.length; i++) {
+            const bar = bars[i];
+            console.log(bar)
+            const li = document.createElement('li');
+            li.innerHTML = `
+            <p class="text"><span> Navn: </span>${bar.bar_name}</p>
+            <p class="bar_location"><span> Lokation: </span>${bar.bar_location}</p>
+            <p class="bar_city"><span> By: </span>${bar.bar_city}</p>
+            <p class="bar_description"><span> Beskrivelse: </span>${bar.bar_description}</p>
+            <p class="opening_hours"><span> Åbningstider: </span>${bar.opening_hours}</p>
+            <p class="prices"><span> Priser: </span>${bar.prices}</p>
+            <p class="games"><span> Spil: </span>${bar.games}</p>
+            <p class="billiard"><span> Billard: </span>${bar.billiard}</p>
+            <p class="smoking_allowed"><span> Rygning indenfor: </span>${bar.smoking_allowed}</p>
+            <p class="happy_hour"><span> Happy Hour: </span>${bar.happy_hour}</p>
+            <p class="viser_sport"><span> Viser sport: </span>${bar.viser_sport}</p>
+        `;
+            ul.appendChild(li);
+        }
+    }
 })
 
 
